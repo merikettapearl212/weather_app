37026ad22305b095aa197761972dd09a5776f5c2
merikettapearl212 <68967132+merikettapearl212@users.noreply.github.com>
2020-11-20 14:06:08 -0800

    updated js

--- Assets/script.js
+++ Assets/script.js
@@ -3,23 +3,31 @@
 //API key 
 var apiKey = "3f5256b0aed6bc757eed5b3080beadb6";
 //build the URL to query database
-var currentDayURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
 var fivedayURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey; 
+
 
 $("#search-form").on("submit", function(event) {
     // since it is a form, we will event.preventDefault() in order to stop the page from refreshing automatically
     event.preventDefault();
     var searchInput = $("#search-input").val();
-    console.log(searchInput);
+    //console.log(searchInput);
+    getCityWeather(searchInput);
+})
+
+function getCityWeather(city) {
+
+    var currentDayURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
     $.ajax({
-      url: currentDayURL + searchInput,
+      url: currentDayURL,
       method: "GET",
     }).then(function (results) {
-      console.log(results);
+      //console.log(results);
       $("#city").text(results.name)
     });
-})
+}
+
 //When input search button clicked
 //store input in local storage
 //GET current weather (temp/wind/humidity/uv index) for city in big card
