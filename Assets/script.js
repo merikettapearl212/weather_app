15749fae7ede2cd2387abf821d25ef5d64a24df2
merikettapearl212 <68967132+merikettapearl212@users.noreply.github.com>
2020-11-20 15:39:56 -0800

    working on uv and winds

--- Assets/script.js
+++ Assets/script.js
@@ -2,6 +2,8 @@
 
 //API key 
 var apiKey = "3f5256b0aed6bc757eed5b3080beadb6";
+//moment()
+var todaysDate = moment().format("MM/DD/YYYY");
 //build the URL to query database
 
 var fivedayURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey; 
@@ -14,21 +16,46 @@ $("#search-form").on("submit", function(event) {
     //console.log(searchInput);
     getCityWeather(searchInput);
 })
+//Get UVINDEX
+/*function UVIndex(){
+  //lets build the url for uvindex.
+  var getuvURL="https://api.openweathermap.org/data/2.5/uvi?appid="+ apiKey+"&lat="+lt+"&lon="+ln;
+  $.ajax({
+          url: getuvURL,
+          method:"GET"
+          }).then(function(response){
+              $("#uvIn").html(response.value);
+          });
+}*/
+
+//city search history
+
 
 function getCityWeather(city) {
 
     var currentDayURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+    console.log(currentDayURL)
 
     $.ajax({
       url: currentDayURL,
       method: "GET",
     }).then(function (results) {
+      
       //console.log(results);
       $("#city").text(results.name)
-      
+      $("#currentDay").text(todaysDate);
+      $("#currentIcon").attr("src", "http://openweathermap.org/img/wn/" + (results.weather[0].icon) + "@2x.png");
+      var farenheitTemp = (results.main.temp - 273.15) * 1.80 + 32;
+      $("#currentTemp").text(farenheitTemp);
+      $("#currentHum").text(results.main.humidity);
+      $("WindS").text(results.wind.speed);
+      //$("#uvIn").text(results)
     });
+
+
 }
 
+//$("#clear-history").on("click",clearHistory);
 //When input search button clicked
 //store input in local storage
 //GET current weather (temp/wind/humidity/uv index) for city in big card
