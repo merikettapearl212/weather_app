7d819a77745c39b04e012f4be8bd6c6764448816
merikettapearl212 <68967132+merikettapearl212@users.noreply.github.com>
2020-11-24 19:18:38 -0800

    working on local storage

--- Assets/script.js
+++ Assets/script.js
@@ -1,58 +1,70 @@
 //onclick search btn
 
-//API key 
+//API key
 var apiKey = "3f5256b0aed6bc757eed5b3080beadb6";
 //moment()
 var todaysDate = moment().format("MM/DD/YYYY");
 //build the URL to query database
 
-var fivedayURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey; 
+var fivedayURL =
+  "https://api.openweathermap.org/data/2.5/forecast?q=" +
+  city +
+  "&appid=" +
+  apiKey;
 
-
-$("#search-form").on("submit", function(event) {
-    // since it is a form, we will event.preventDefault() in order to stop the page from refreshing automatically
-    event.preventDefault();
-    var searchInput = $("#search-input").val();
-    //console.log(searchInput);
-    getCityWeather(searchInput);
-})
+$("#search-form").on("submit", function (event) {
+  // since it is a form, we will event.preventDefault() in order to stop the page from refreshing automatically
+  event.preventDefault();
+  var searchInput = $("#search-input").val();
+  //console.log(searchInput);
+  getCityWeather(searchInput);
+});
 //Get UVINDEX
-/*function UVIndex(){
+function UVIndex(lt, ln) {
   //lets build the url for uvindex.
-  var getuvURL="https://api.openweathermap.org/data/2.5/uvi?appid="+ apiKey+"&lat="+lt+"&lon="+ln;
+  var getuvURL =
+    "https://api.openweathermap.org/data/2.5/uvi?appid=" +
+    apiKey +
+    "&lat=" +
+    lt +
+    "&lon=" +
+    ln;
   $.ajax({
-          url: getuvURL,
-          method:"GET"
-          }).then(function(response){
-              $("#uvIn").html(response.value);
-          });
-}*/
+    url: getuvURL,
+    method: "GET",
+  }).then(function (response) {
+    $("#uvIn").text(response.value);
+  });
+}
 
 //city search history
 
-
 function getCityWeather(city) {
+  var currentDayURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    apiKey;
+  console.log(currentDayURL);
 
-    var currentDayURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
-    console.log(currentDayURL)
+  $.ajax({
+    url: currentDayURL,
+    method: "GET",
+  }).then(function (results) {
+    $("#city").text(results.name);
+    $("#currentDay").text(todaysDate);
+    $("#currentIcon").attr(
+      "src",
+      "http://openweathermap.org/img/wn/" + results.weather[0].icon + "@2x.png"
+    );
+    var farenheitTemp = (results.main.temp - 273.15) * 1.8 + 32;
+    $("#currentTemp").text(farenheitTemp);
+    $("#currentHum").text(results.main.humidity);
 
-    $.ajax({
-      url: currentDayURL,
-      method: "GET",
-    }).then(function (results) {
-      
-      //console.log(results);
-      $("#city").text(results.name)
-      $("#currentDay").text(todaysDate);
-      $("#currentIcon").attr("src", "http://openweathermap.org/img/wn/" + (results.weather[0].icon) + "@2x.png");
-      var farenheitTemp = (results.main.temp - 273.15) * 1.80 + 32;
-      $("#currentTemp").text(farenheitTemp);
-      $("#currentHum").text(results.main.humidity);
-      //$("WindS").text(results.wind.speed);
-      //$("#uvIn").text(results)
-    });
-
-
+    $("#WindS").text(results.wind.speed);
+    //$("#uvIn").text(results)
+    UVIndex(results.coord.lat, results.coord.lon);
+  });
 }
 
 //$("#clear-history").on("click",clearHistory);
@@ -61,3 +73,7 @@ function getCityWeather(city) {
 //GET current weather (temp/wind/humidity/uv index) for city in big card
 //append to each in html
 //get future dates and weather for each card
+//value = "value"
+//var value = localStorage.getItem("key");
+console.log(value);
+localStorage.setItem("socks", "christmas");
